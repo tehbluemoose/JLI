7bd43b814a51f67f41a064a0352c33c04ad1e37d
tehbluemoose <njpreynolds@gmail.com>
2017-01-25 18:33:50 -0600

    a

---
+++ language.java
@@ -0,0 +1,41 @@
+import java.util.scanner;
+
+class Main{	
+	public static void main(String[] args){
+		System.out.println("ooo");
+	}
+}
+class InReader{
+	Sentence previous = Null;	
+}
+class Sentence{
+	
+}
+class Word{
+	String w;
+	Function f;
+}
+class Verb extends Word{
+
+}
+class Noun extends Word{
+
+}
+class Adjective extends Word{
+
+}
+class Adverb extends Word{
+
+}
+class Conjunction extends Word{
+
+}
+class Preposition extends Word{
+
+}
+class Exclamation extends Word{
+
+}
+class Onomatopoeia extends Word{
+
+}
